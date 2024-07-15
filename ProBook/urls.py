46be3ca5225b9cac67a7a6bb3a34c9aa4e10421d
phantu279999 <phantu279999@gmail.com>
2024-07-15 14:33:55 +0700

    update base project

--- ProBook/urls.py
+++ ProBook/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
+from django.conf import settings
 
 urlpatterns = [
+	path('', include('myapp.urls')),
     path('admin/', admin.site.urls),
 ]
+
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
