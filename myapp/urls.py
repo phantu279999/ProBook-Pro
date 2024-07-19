01650da9ddd523ad392ef1c192d388ce90e69c59
phantu279999 <phantu279999@gmail.com>
2024-07-19 15:48:52 +0700

    update code

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
 	path("", views.index, name='index'),
 	path("video-youtube/", views.crawl_video_youtube, name='video_youtube'),
+	path("extract-format/", views.extract_format, name='extract_format'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -2,6 +2,8 @@ import os
 import sys
 
 from django.shortcuts import render
+from django.urls import reverse
+from django.http.response import HttpResponseRedirect
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
@@ -12,21 +14,46 @@ from src.common.common import write_data_video_to_file_csv
 
 def index(request):
 	context = {}
-	if request.method == 'POST':
+	if request.method == 'POST' and 'your_domain' in request.POST:
 		your_domain = request.POST['your_domain']
 		res = ProcessSEO().process_single_link(your_domain)
 		context['res'] = res
+	elif request.method == 'POST' and 'channel_youtube' in request.POST:
+		channel_ytb = request.POST['channel_youtube']
+		redirect_url = reverse('video_youtube')
+		redirect_url += '?channel_ytb={}'.format(channel_ytb)
+		return HttpResponseRedirect(redirect_url)
+
 	return render(request, 'index.html', context=context)
 
 
 def crawl_video_youtube(request):
 	context = {}
+	channel_ytb = ""
 	if request.method == 'POST':
-		domain_channel = request.POST['domain_channel']
-		list_video = GetVideoYoutube().app_run(domain_channel)
-		res = write_data_video_to_file_csv(list_video)
-		if res == 'Error':
-			print("Error when try write to file csv")
+		channel_ytb = request.POST['domain_channel']
+	elif 'channel_ytb' in request.GET and request.GET['channel_ytb']:
+		channel_ytb = request.GET['channel_ytb']
+
+	if channel_ytb:
+		list_video, status = get_list_video_ytb(channel_ytb)
 		context['list_video'] = list_video
+		context['status'] = status
 		context['length'] = len(list_video)
+
 	return render(request, 'video_youtube.html', context=context)
+
+
+def extract_format(request):
+	context = {}
+	return render(request, 'extract_format.html', context=context)
+
+
+def get_list_video_ytb(link_channel):
+	list_video = GetVideoYoutube().app_run(link_channel)
+	res = write_data_video_to_file_csv(list_video)
+	status = True
+	if res == 'Error':
+		print("Error when try write to file csv")
+		status = False
+	return list_video, status
