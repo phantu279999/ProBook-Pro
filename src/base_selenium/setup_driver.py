638dfa01cfe9a2ce512598ada3aede5695a98983
phantu279999 <phantu279999@gmail.com>
2024-07-30 15:26:51 +0700

    performance driver selenium

--- src/base_selenium/setup_driver.py
+++ src/base_selenium/setup_driver.py
@@ -4,33 +4,40 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 import chromedriver_autoinstaller
 
-
 # auto download chromedriver
 chromedriver_autoinstaller.install()
 
+
 def custom_chrome():
-    options_chrome = Options()
-    options_chrome.page_load_strategy = "eager"
-    options_chrome.add_argument("--start-maximized")
-    options_chrome.add_experimental_option('excludeSwitches', ['enable-logging'])
+	options_chrome = Options()
+	options_chrome.page_load_strategy = "eager"
+	options_chrome.add_argument("--start-maximized")
+	options_chrome.add_experimental_option('excludeSwitches', ['enable-logging'])
 
-    driver = webdriver.Chrome(options=options_chrome)
+	driver = webdriver.Chrome(options=options_chrome)
 
-    driver.implicitly_wait(30)
+	driver.implicitly_wait(30)
 
-    return driver
+	return driver
 
 
 def custome_chrome_headless():
-    options_chrome = Options()
-    options_chrome.page_load_strategy = "eager"
-    options_chrome.add_argument("--start-maximized")
-    options_chrome.add_argument("--headless")
-    options_chrome.add_argument('--ignore-certificate-errors')
-    options_chrome.add_argument('--ignore-ssl-errors')
-    options_chrome.add_experimental_option('excludeSwitches', ['enable-logging'])
+	options_chrome = Options()
+	options_chrome.page_load_strategy = "eager"
+	options_chrome.add_argument("--start-maximized")
+	options_chrome.add_argument("--headless")
+	options_chrome.add_argument('--ignore-certificate-errors')
+	options_chrome.add_argument('--ignore-ssl-errors')
+	options_chrome.add_argument('--disable-extensions')
+	options_chrome.add_argument('--no-sandbox')
+	options_chrome.add_argument('--disable-dev-shm-usage')
+	options_chrome.add_argument('--incognito')
+	options_chrome.add_argument("--disable-background-networking")
+	options_chrome.add_argument("--disable-notifications")
+	options_chrome.add_argument('--disable-infobars')
+	options_chrome.add_experimental_option('excludeSwitches', ['enable-logging'])
 
-    driver = webdriver.Chrome(options=options_chrome)
-    driver.implicitly_wait(30)
+	driver = webdriver.Chrome(options=options_chrome)
+	driver.implicitly_wait(30)
 
-    return driver
+	return driver
