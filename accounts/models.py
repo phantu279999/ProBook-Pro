b850022546f72d8ce4f92562df5b6d63f2ffbd37
phantu279999 <phantu279999@gmail.com>
2024-08-07 10:52:21 +0700

    update app accounts

--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.utils.translation import gettext_lazy as _
 
+
 class User(AbstractUser):
     username = models.CharField(max_length=50, blank=True, null=True, unique=True)
     email = models.EmailField(_('email address'), unique=True)
